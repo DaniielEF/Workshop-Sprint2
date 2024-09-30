bea2b80e35acf69258d3ddc61216bd5989723354
DanielErazo <daniel96.florez@gmail.com>
2024-09-30 15:50:49 -0500

    layout

---
+++ socialMedia/src/containers/BottomNavigationBar.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import SearchIcon from '@mui/icons-material/Search';
+import RestoreIcon from '@mui/icons-material/Restore';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+
+
+const BottomNavigationBar = () => {
+  const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+
+    // Navegación por las rutas correspondientes
+    switch (newValue) {
+      case 0:
+        navigate('/home'); 
+        break;
+      case 1:
+        navigate('/search'); 
+        break;
+      case 2:
+        navigate('/orderHistory'); 
+        break;
+      case 3:
+        navigate('/profile'); 
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={handleChange}
+      showLabels
+      sx={{ width: '100%', position: 'fixed', bottom: 0 }}
+    >
+      <BottomNavigationAction  icon={<HomeOutlinedIcon />} />
+      <BottomNavigationAction  icon={<SearchIcon />} />
+      <BottomNavigationAction  icon={<RestoreIcon />} />
+      <BottomNavigationAction  icon={<PersonOutlineOutlinedIcon />} />
+    </BottomNavigation>
+  );
+}
+
+export default BottomNavigationBar;
--- socialMedia/src/routes/AppRoutes.jsx
+++ socialMedia/src/routes/AppRoutes.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import BowserRouter, { Route, Routes } from 'react-router-dom'
 import Home from '../components/Home'
+import Post from '../components/Post'
 
 const AppRoutes = () => {
   return (
     <BowserRouter>
         <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route path="/*" element={<Layout />} >
+        </Route>
         </Routes>
     </BowserRouter>
   )
 }
+
+const Layout = () => (
+    <>
+      <BottomNavigationBar />
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="post" element={<Post />} />
+      </Routes>
+    </>
+  );
 
 export default AppRoutes
