2600bd7f488575d6ca466ed898bfd859a249401a
DanielErazo <daniel96.florez@gmail.com>
2024-09-30 16:06:49 -0500

    home2

--- socialMedia/src/components/Home.jsx
+++ socialMedia/src/components/Home.jsx
@@ -1,9 +1,13 @@
+import { Card } from '@mui/material'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
         <h1>Profiles</h1>
+        <Card>
+            Content
+        </Card>
     </div>
   )
 }
